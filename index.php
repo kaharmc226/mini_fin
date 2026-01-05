5d3b5e1ea1658066ab201cc76e7c73e935488358
kahar_crunchycloud <klencoc226@gmail.com>
2026-01-05 14:47:05 +0900

    Nuke

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/helpers.php';
-if (isset($_SESSION['user'])) {
-    header('Location: dashboard.php');
-    exit;
-}
-header('Location: login.php');
-exit;
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/helpers.php';
-session_destroy();
-header('Location: login.php');
-exit;
